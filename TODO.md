40aff0b063ad50b8d728ea39014745c7d04a5218
Matthew Smith <matt@matthewsmith.com>
2019-12-22 08:21:54 -0600

    add license and gulp tasks

--- TODO.md
+++
@@ -1,5 +0,0 @@
-# TODO before beta
-- Update colors on create flow
-- Remove alerts
-- Remove unnecessary console.log
-- CSS for blockquote / code (pre)
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,9 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var minifyCss = require('gulp-minify-css');
-var gulpSequence = require('gulp-sequence')
-
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    minifyCss = require('gulp-minify-css')
 
 // copy and combine css files
-gulp.task('css', function(done) {
+gulp.task('css-app', function(done) {
 
     let location = 'resources/site/css'
 
@@ -13,7 +11,7 @@ gulp.task('css', function(done) {
     gulp.src([`${location}/variables.css`, `${location}/custom.css`, `${location}/footer.css`, `${location}/header.css`, `${location}/hero.css`, `${location}/layout.css`, `${location}/site.css`, `${location}/top.css`, `${location}/widgets.css`])
         .pipe(concat('site.all.css'))
         .pipe(minifyCss())
-        .pipe(gulp.dest(location));
+        .pipe(gulp.dest(location))
 
     done()
   
@@ -21,18 +19,52 @@ gulp.task('css', function(done) {
 
 
 // copy and combine js files
-gulp.task('js', function(done) {
+gulp.task('js-app', function(done) {
 
     let location = 'resources/site/js'
 
     // concat js
     gulp.src([`${location}/widgets.js`])
         .pipe(concat('site.all.js'))
-        .pipe(gulp.dest(location));
+        .pipe(gulp.dest(location))
   
-    done();
+    done()
   
   });
 
-// run tasks
-gulp.task('default', gulp.parallel('css', 'js'));
+
+// copy and combine css files
+gulp.task('css-site', function(done) {
+
+    let location = 'site/css'
+
+    // concat css
+    gulp.src([`${location}/variables.css`, `${location}/custom.css`, `${location}/footer.css`, `${location}/header.css`, `${location}/hero.css`, `${location}/layout.css`, `${location}/site.css`, `${location}/top.css`, `${location}/widgets.css`])
+        .pipe(concat('site.all.css'))
+        .pipe(minifyCss())
+        .pipe(gulp.dest(location))
+
+    done()
+  
+  });
+
+
+// copy and combine js files
+gulp.task('js-site', function(done) {
+
+    let location = 'site/js'
+
+    // concat js
+    gulp.src([`${location}/widgets.js`])
+        .pipe(concat('site.all.js'))
+        .pipe(gulp.dest(location))
+  
+    done()
+  
+  });
+
+// app
+gulp.task('default', gulp.parallel('css-app', 'js-app'))
+
+// site
+gulp.task('site', gulp.parallel('css-site', 'js-site'))
